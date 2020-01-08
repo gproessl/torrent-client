649dc8734754b0123b784bea73f505ad8abeedb2
gproessl <gerrit.proessl@gmail.com>
2020-01-08 14:30:45 +0100

    add: usage fn

--- main.go
+++ main.go
@@ -7,7 +7,16 @@ import (
 	"github.com/gproessl/torrent-client/torrent"
 )
 
+func printUsage() {
+	log.Println("usage: torrent-client torrentfile outfile")
+}
+
 func main() {
+	if len(os.Args) < 3 {
+		printUsage()
+		return
+	}
+
 	inPath := os.Args[1]
 	outPath := os.Args[2]
 
